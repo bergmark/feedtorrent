fc149c43a7da78bd462b41153aad64f93dd8a871
Raevel <github.com@edea.se>
2009-12-31 22:43:23 +0100

    Added command line argument parsing with parseargs.

---
+++ Adam/FeedTorrent/Args.hs
@@ -0,0 +1,54 @@
+module Adam.FeedTorrent.Args (parseArgs) where
+
+import System.Console.ParseArgs hiding (parseArgs)
+
+import Adam.FeedTorrent.PreludeImports
+
+parseArgs :: IO AppArgs
+parseArgs = do
+  res <- parseArgsIO ArgsComplete [argConfig, argCommandFeeds, argCommandTorrents, argCommandTest]
+  return AppArgs { arg_configFile = stringArg res "config-file"
+                 , arg_commandFeeds = optionalArgToBool res "feeds"
+                 , arg_commandTorrents = optionalArgToBool res "torrents"
+                 , arg_commandTest = optionalArgToBool res "test"
+                 }
+
+stringArg :: Args String -> String -> String
+stringArg res index = fromJust (getArg res index)
+
+optionalArgToBool :: Args String -> String -> Bool
+optionalArgToBool res index = not . isNothing $ (getArg res index :: Maybe String)
+
+argConfig :: Arg String
+argConfig = Arg { argIndex = "config-file"
+                , argName = Just "config-file"
+                , argAbbr = Just 'c'
+                , argData = requiredArg
+                , argDesc = "Config file location." }
+
+argCommandFeeds :: Arg String
+argCommandFeeds = commandArg "feeds" (Just 'f') "Fetches feed files specified in config."
+
+argCommandTorrents :: Arg String
+argCommandTorrents = commandArg "torrents" (Just 't') "Fetches all torrents inside any unprocessed RSS documents."
+
+argCommandTest :: Arg String
+argCommandTest = commandArg "test" Nothing "Runs a few tests."
+
+commandArg :: String -> Maybe Char -> String -> Arg String
+commandArg argName' abbr desc = Arg {
+  argIndex = argName'
+  , argName = Just argName'
+  , argAbbr = abbr
+  , argData = Nothing
+  , argDesc = desc
+  }
+
+requiredArg :: Maybe DataArg
+requiredArg = argDataRequired "string" (ArgtypeString . fromJust)
+
+data AppArgs = AppArgs { arg_configFile :: String
+                       , arg_commandFeeds :: Bool
+                       , arg_commandTorrents :: Bool
+                       , arg_commandTest :: Bool
+                       } deriving (Show, Read)
